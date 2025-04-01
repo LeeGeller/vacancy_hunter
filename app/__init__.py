92cdcbbd75e26ba0f27ec438b93caa7f93586b58
Kris <ssspers92@gmail.com>
2025-04-01 19:57:06 +0300

    Created fabric method

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Confg
+
+db = SQLAlchemy()
+
+def create_app():
+
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
+    from app.views.import main
+    app.register_blueprint(main)
+
+    return app
