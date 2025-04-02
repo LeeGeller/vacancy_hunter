40a97b4fd87394d0d5e74fb3844553f8ed495ae7
Kris <ssspers92@gmail.com>
2025-04-02 22:20:09 +0300

    Added params for database

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Vacancy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255),)
+    description = db.Column(db.Text,)
+    schedule_type = db.Column(db.String(255))
+    salary_from = db.Column(db.Integer,)
+    salary_to = db.Column(db.Integer,)
+    url = db.Column(db.String)
