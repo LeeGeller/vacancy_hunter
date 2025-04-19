b8106a3ee71821b1142215e15a7c517bf1a3dc05
Kris <ssspers92@gmail.com>
2025-04-20 01:24:44 +0300

    Added info about func build_url_and_headers, rename pars_url to pars_vacancies. Created child class HabrParsingVacancy with build_url_and_headers

--- app/scrapper.py
+++ app/scrapper.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
 
-import requests
-
 
 class AbstractParsingVacancy(ABC):
     def __init__(self, query_vacancies: str, area: int, page_limit: int):
@@ -15,10 +13,14 @@ class AbstractParsingVacancy(ABC):
 
     @abstractmethod
     def build_url_and_headers(self):
+        """
+        Prepare url and query_params for parsing
+        :return: query_url and query_params
+        """
         pass
 
     @abstractmethod
-    def pars_url(self):
+    def pars_vacancies(self):
         pass
 
 
@@ -30,10 +32,6 @@ class HHParsingVacancy(AbstractParsingVacancy):
         self.build_url_and_headers()
 
     def build_url_and_headers(self):
-        """
-        Prepare url and query_params for parsing
-        :return: query_url and query_params
-        """
         self.query_url = f"https://api.hh.ru/vacancies"
         self.query_params = {
             "text": self.query_vacancies,
@@ -41,8 +39,24 @@ class HHParsingVacancy(AbstractParsingVacancy):
             "per_page": self.page_limit,
         }
 
-        # if 'habr' in self.url:
-        #     self.url = "https://career.habr.com/vacancies?q=" + self.query_vacancies
-        #     self.query_params = {
-        #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        #     }
+    # def pars_vacancies(self):
+    #     vacancies_response = []
+    #     async with session.get(url, params=keys_response) as response:
+    #         if response.status == 200:
+    #             return await response.json()  # Возвращаем данные, если запрос успешен
+    #         else:
+    #             print(f"Failed to retrieve vacancies from {url}. Status code: {response.status}")
+
+
+class HabrParsingVacancy(AbstractParsingVacancy):
+    def __init__(self, query_vacancies: str, area: int, page_limit: int):
+        super().__init__(query_vacancies, area, page_limit)
+        self.query_url: str = ''
+        self.query_params: dict = {}
+        self.build_url_and_headers()
+
+    def build_url_and_headers(self):
+        self.query_url = "https://career.habr.com/vacancies?q=" + self.query_vacancies
+        self.query_params = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
