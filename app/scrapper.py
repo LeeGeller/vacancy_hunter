1458f8293427fd50ba76883e106e19770cef7dc1
Kris <ssspers92@gmail.com>
2025-04-20 00:20:17 +0300

    Created two classes: Abstract and for HH

--- app/scrapper.py
+++ app/scrapper.py
@@ -1,27 +1,48 @@
+from abc import ABC, abstractmethod
+
 import requests
 
 
-class ParsingVacancy:
-    def __init__(self, url: str, query_vacancies_list: list[str], area: int, page_limit: int):
-        self.url = url
-        self.query_vacancies_list = query_vacancies_list
-        self.area = area
-        self.page_limit = page_limit
+class AbstractParsingVacancy(ABC):
+    def __init__(self, query_vacancies: str, area: int, page_limit: int):
+        self.query_vacancies: str = query_vacancies
+        self.area: int = area
+        self.page_limit: int = page_limit
+        self.vacancies: list[dict] = []
 
     def __repr__(self):
-        return f'Vacancy from: {self.url}'
+        return f'Vacancy for: {self.query_vacancies}'
 
-    def check_url(self, url):
+    @abstractmethod
+    def build_url_and_headers(self):
+        pass
 
-
+    @abstractmethod
     def pars_url(self):
+        pass
 
-        response = requests.get(self.url)
 
-        keys_response = {
-            "text": " ".join(self.query_vacancies_list),
+class HHParsingVacancy(AbstractParsingVacancy):
+    def __init__(self, query_vacancies: str, area: int, page_limit: int):
+        super().__init__(query_vacancies, area, page_limit)
+        self.query_url: str = ''
+        self.query_params: dict = {}
+        self.build_url_and_headers()
+
+    def build_url_and_headers(self):
+        """
+        Prepare url and query_params for parsing
+        :return: query_url and query_params
+        """
+        self.query_url = f"https://api.hh.ru/vacancies"
+        self.query_params = {
+            "text": self.query_vacancies,
             "area": self.area,
-            "per_page": 100,
+            "per_page": self.page_limit,
         }
 
-        vacancies = []
+        # if 'habr' in self.url:
+        #     self.url = "https://career.habr.com/vacancies?q=" + self.query_vacancies
+        #     self.query_params = {
+        #         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        #     }
